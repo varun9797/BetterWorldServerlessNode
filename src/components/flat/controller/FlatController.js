e111430a9d5e0e16c5aa72e9cdce56e5adfd6796
varun-perpetualny <varun@perpetualny.com>
2019-06-28 19:52:45 +0530

    added aws utility for upload and download file

--- src/components/flat/controller/FlatController.js
+++ src/components/flat/controller/FlatController.js
@@ -1,5 +1,6 @@
 import flatModel from "../model/FlatModel"
 import responseFormat from "../../../../lib/response-format"
+import awsUtility from "../../utility/awsUtility";
 
 class FlatController {
     constructor(){
@@ -15,6 +16,35 @@ class FlatController {
             res.status(responseFormat.statusCode["SUCCESS"]).json(responseFormat.getExpressResponseObject("success", responseFormat.statusCode["SUCCESS"], "function executed successfully!", result));
         } catch(err) {
             console.log("FlatController :: getFlatsByOwnerId :: Error", err);
+            res.status(responseFormat.statusCode["INTERNAL_SERVER_ERROR"]).json(responseFormat.getExpressResponseObject("error", responseFormat.statusCode["INTERNAL_SERVER_ERROR"], "Something went wrong!", err.message));
+        } 
+    } 
+
+    uploadFileOnS3 = async (req, res) => {
+        try {
+            console.log("FlatController :: uploadFileOnS3");
+            let result = await awsUtility.uploadToS3(req.body.image);
+            res.status(responseFormat.statusCode["SUCCESS"]).json(responseFormat.getExpressResponseObject("success", responseFormat.statusCode["SUCCESS"], "function executed successfully!", result));
+        } catch(err) {
+            console.log("FlatController :: uploadFileOnS3 :: Error", err);
+
+            res.status(responseFormat.statusCode["INTERNAL_SERVER_ERROR"]).json(responseFormat.getExpressResponseObject("error", responseFormat.statusCode["INTERNAL_SERVER_ERROR"], "Something went wrong!", err.message));
+        } 
+    } 
+
+    getFileFromS3 = async (req, res) => {
+        try {
+            console.log("FlatController :: uploadFileOnS3");
+            let result = await awsUtility.getS3Image(req.query);
+            res.writeHead(200, { 'Content-Type': 'image/png' });
+            res.write(result, 'binary');
+            res.end(null, 'binary');
+            // console.log("***********");
+            // res.download(result);
+            //res.status(responseFormat.statusCode["SUCCESS"]).json(responseFormat.getExpressResponseObject("success", responseFormat.statusCode["SUCCESS"], "function executed successfully!", result));
+        } catch(err) {
+            console.log("FlatController :: uploadFileOnS3 :: Error", err);
+
             res.status(responseFormat.statusCode["INTERNAL_SERVER_ERROR"]).json(responseFormat.getExpressResponseObject("error", responseFormat.statusCode["INTERNAL_SERVER_ERROR"], "Something went wrong!", err.message));
         } 
     } 
--- src/components/flat/router/FlatRouter.js
+++ src/components/flat/router/FlatRouter.js
@@ -8,5 +8,7 @@ import flatController from "../controller/FlatController";
 // router.get("/ownerFlats", authenticationController.verifyTokenMiddleware, flatController.getFlatsByOwnerId);
 // router.get("/societyFlats", authenticationController.verifyTokenMiddleware, flatController.getFlatsBySocietyId);
 router.get("/getFlats", flatController.getFlats);
+router.post("/uploadFile", flatController.uploadFileOnS3);
+router.get("/getFile", flatController.getFileFromS3);
 
   export default router;
