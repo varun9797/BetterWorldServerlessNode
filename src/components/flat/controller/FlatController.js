2b3e2dd757f4827633347fb1a472513302c8551a
varun-perpetualny <varun@perpetualny.com>
2019-07-12 20:08:38 +0530

    Changes for file upload functionality using multiparty

--- src/components/flat/controller/FlatController.js
+++ src/components/flat/controller/FlatController.js
@@ -1,7 +1,7 @@
 import flatModel from "../model/FlatModel"
 import responseFormat from "../../../../lib/response-format"
 import awsUtility from "../../utility/awsUtility";
-
+import multiparty from "multiparty";
 class FlatController {
     constructor(){
         console.log("inside FlatController");
@@ -47,32 +47,63 @@ class FlatController {
 
     uploadFileOnS3 = async (req, res) => {
         try {
-            if(req.body.flatId) {
-                let flatId = req.body.flatId;
-                console.log("FlatController :: uploadFileOnS3");
-            let s3ResponsePromiseArray = [];
-            let dbResponsePromiseArray =[];
 
-            req.body.fileDetailArray.forEach((data)=>{
-                let base64String = data.base64ImageData.split(',').pop();
-                data.fileName = "Owner-"+req.body.senderInfo.ownerid+"-Flat-"+flatId+"-"+data.fileName
-                let result = awsUtility.uploadToS3(base64String, data.fileName);
-                s3ResponsePromiseArray.push(result);
-            })
+            var form = new multiparty.Form();
+            form.parse(req, async (err, fields, files) => {
+                if (err) {
+                    console.log(`ListingValidation : saveDocument :: ${JSON.stringify(err)}`);
+                    res.status(responseFormat.statusCode["BAD_REQUEST"]).send(responseFormat.getResponseObject("error", responseFormat.statusCode["BAD_REQUEST"], " Multiparty data format not match", err));
+    
+                } 
 
-            let s3ResponseArr = await Promise.all(s3ResponsePromiseArray);
-            console.log("******s3ResponseArr******",s3ResponseArr);
-            s3ResponseArr.forEach((data)=>{
-                data.flatId = flatId;
-                let result = this.insertFlatFiles(data);
-                dbResponsePromiseArray.push(result);
-            })
-            let dbResponseArr = await Promise.all(dbResponsePromiseArray);
-            console.log("******dbResponseArr******",dbResponseArr);
-            res.status(responseFormat.statusCode["SUCCESS"]).json(responseFormat.getExpressResponseObject("success", responseFormat.statusCode["SUCCESS"], "function executed successfully!", dbResponseArr));
-            } else {
-                throw new Error("Please provide Flat Id");
-            }
+                // let parsedFileObject = JSON.parse(fields.fileInfo[0]);
+                // console.log("This else fields ", parsedFileObject);
+                // let fileDecriptionArray = parsedFileObject.fileArray;
+                // let externalDetails = parsedFileObject.externalFileDetails;
+                // let flatId = externalDetails.flatId
+                let fileObject = files
+                
+                console.log("This else files ", JSON.stringify(fileObject));
+                // let s3ResponsePromiseArray = [];
+                // let dbResponsePromiseArray =[];
+                // fileDecriptionArray.forEach((fieldDescription)=>{
+                //     console.log("&&&&&&");
+                //     console.log("**********",fileObject[fieldDescription.file_name]);
+                //     let fileName = "Owner-"+req.body.senderInfo.ownerid+"-Flat-"+flatId+"--"+fieldDescription.doc_name+"."+fieldDescription.file_name.split(".")[1];
+                //     let result = awsUtility.uploadToS3(fileObject[fieldDescription.file_name][0], fileName);
+                //     s3ResponsePromiseArray.push(result);
+                // })
+               // if(req.body.flatId) {
+                   // let flatId = req.body.flatId;
+                    console.log("FlatController :: uploadFileOnS3");
+                // let s3ResponsePromiseArray = [];
+                // let dbResponsePromiseArray =[];
+
+
+    
+                // files.forEach((data)=>{
+                //     let base64String = data.base64ImageData.split(',').pop();
+                //     data.fileName = "Owner-"+req.body.senderInfo.ownerid+"-Flat-"+flatId+"-"+data.fileName
+                //     let result = awsUtility.uploadToS3(base64String, data.fileName);
+                //     s3ResponsePromiseArray.push(result);
+                // })
+    
+                // let s3ResponseArr = await Promise.all(s3ResponsePromiseArray);
+                // console.log("******s3ResponseArr******",s3ResponseArr);
+                // s3ResponseArr.forEach((data)=>{
+                //     data.flatId = flatId;
+                //     let result = this.insertFlatFiles(data);
+                //     dbResponsePromiseArray.push(result);
+                // })
+                // let dbResponseArr = await Promise.all(dbResponsePromiseArray);
+                // console.log("******dbResponseArr******",dbResponseArr);
+                res.status(responseFormat.statusCode["SUCCESS"]).json(responseFormat.getExpressResponseObject("success", responseFormat.statusCode["SUCCESS"], "function executed successfully!", "All Ok"));
+                // } else {
+                //     throw new Error("Please provide Flat Id");
+                // }
+    
+            });
+
             
         } catch(err) {
             console.log("FlatController :: uploadFileOnS3 :: Error", err);
--- src/components/utility/awsUtility.js
+++ src/components/utility/awsUtility.js
@@ -1,20 +1,25 @@
 import AWS from 'aws-sdk';
+import fs from "fs";
  const BUCKET_NAME = "betterworldimage";
+ import mime from "mime-types";
+ import path from "path";
 // const IAM_USER_KEY = process.env.AWS_ACCESS_KEY;
 // const IAM_USER_SECRET = process.env.AWS_SECRET_ACCESS_KEY;
 AWS.config.update({ region: 'us-east-1' });
 let s3 = new AWS.S3();
-var uploadToS3= function(imageData, filename){
+var uploadToS3= function(fileObject, filename){
     return new Promise((resolve, rejects)=>{
-        let encodedImage = imageData;
-        let decodedImage = Buffer.from(encodedImage, 'base64');
+        //let encodedImage = imageData;
+        //let decodedImage = Buffer.from(encodedImage, 'base64');
         let filePath = "user/flat/" + filename
-    
+        
         let params = {
-            "Body": decodedImage,
+            "Body": fs.createReadStream(fileObject.path),
             "Bucket": BUCKET_NAME,
-            "Key": filePath
+            "Key": filePath,
+            "ContentType": mime.contentType(path.extname(filePath)) || ""
         };
+        console.log("*path is ---", params);
         s3.upload(params, function (err, data) {
             if (err) {
                 console.log("awsUtility :: uploadToS3 :: Error", err);
